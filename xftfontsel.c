f9c6619ff002b0eb8cbec9a37232d151d61ed94c
jaredy <>
2004-04-22 01:32:58 +0000

    more initial work

--- xftfontsel.c
+++ xftfontsel.c
@@ -1,14 +1,15 @@
 #include <err.h>
 
-#include <X11/Xaw/Viewport.h>
+#include <X11/Intrinsic.h>
+#include <X11/StringDefs.h>
 #include <X11/Xaw/AsciiText.h>
 #include <X11/Xaw/Box.h>
-#include <X11/Xaw/Paned.h>
-#include <X11/Xaw/Label.h>
-#include <X11/Xaw/Command.h>
 #include <X11/Xaw/Cardinals.h>
-
-#include <X11/StringDefs.h>
+#include <X11/Xaw/Command.h>
+#include <X11/Xaw/Label.h>
+#include <X11/Xaw/MenuButton.h>
+#include <X11/Xaw/Paned.h>
+#include <X11/Xaw/Viewport.h>
 #include <X11/Xft/Xft.h>
 #include <X11/extensions/Xrender.h>
 #include <fontconfig/fontconfig.h>
@@ -28,33 +29,39 @@ struct ftprops {
 	Widget		 prop_widget;
 	char		*prop_desc;
 } props[] = {
-	{ "family",		"family",	T_STR,		NULL, "Font family name" },
-	{ "style",		"style",	T_STR,		NULL, "Font style (overrides weight/slant)" },
-	{ "slant",		"slant",	T_INT,		NULL, "Italics/roman/oblique" },
-	{ "weight",		"weight",	T_INT,		NULL, "Light/medium/bold" },
-	{ "size",		"size",		T_DBL,		NULL, "Point size" },
-	{ "aspect",		"aspect",	T_DBL,		NULL, "Horizontal stretch" },
-	{ "pixelsize",		"pixelsize",	T_DBL,		NULL, "Pixel size" },
-	{ "spacing",		"spacing",	T_INT,		NULL, "Proprotional/monospace/charcell" },
-	{ "foundry",		"foundry",	T_STR,		NULL, "Font foundry name" },
-	{ "antialias",		"aa",		T_BOOL,		NULL, "Antialiasing" },
-	{ "hinting",		"hint",		T_BOOL,		NULL, "Hinting" },
-	{ "verticallayout",	"vertlayout",	T_BOOL,		NULL, "Vertical layout" },
-	{ "autohint",		"autohint",	T_BOOL,		NULL, "Autohinter" },
-	{ "globaladvance",	"globaladv",	T_BOOL,		NULL, "Global advance data" },
-	{ "file",		"file",		T_STR,		NULL, "System filename" },
-	{ "index",		"index",	T_INT,		NULL, "Index of font from resource" },
-	{ "ftface",		"ftface",	T_FT_FACE,	NULL, "FreeType face" },
-	{ "rasterizer",		"rasterizer",	T_STR,		NULL, "Rasterizer" },
-	{ "outline",		"outline",	T_BOOL,		NULL, "Outlines" },
-	{ "scalable",		"scalable",	T_BOOL,		NULL, "Scalibility" },
-	{ "scale",		"scale",	T_DBL,		NULL, "Point to pixel scale conversion" },
-	{ "dpi",		"dpi",		T_DBL,		NULL, "Dots per inch" },
-	{ "rgba",		"rgba",		T_INT,		NULL, "Subpixel geometry" },
-	{ "minspace",		"minspace",	T_BOOL,		NULL, "Line space leading" },
-	{ "charset",		"charset",	T_CHARSET,	NULL, "Character set" },
-	{ "lang",		"lang",		T_STR,		NULL, "Languages" },
+	/* core, xlfn */
+	{ XFT_ANTIALIAS,	"aa",		T_BOOL,		NULL, "Antialiasing" },
+/*	{ XFT_ASPECT,		"aspect",	T_DBL,		NULL, "Horizontal stretch" }, */
+/*	{ XFT_AUTOHINT,		"autohint",	T_BOOL,		NULL, "Autohinter" }, */
+	{ XFT_CHAR_WIDTH,	"charwidth",	T_INT,		NULL, "Character width"},
+	{ XFT_CHAR_HEIGHT,	"charheight",	T_INT,		NULL, "Character height"},
+/*	{ XFT_CHARSET,		"charset",	T_CHARSET,	NULL, "Character set" }, */
+	{ XFT_DPI,		"dpi",		T_DBL,		NULL, "Dots per inch" },
+/*	{ XFT_ENCODING,		"encoding",	T_DBL,		NULL, "???" }, */
+	{ XFT_FAMILY,		"family",	T_STR,		NULL, "Font family name" },
+	{ XFT_FILE,		"file",		T_STR,		NULL, "System filename" },
+	{ XFT_FOUNDRY,		"foundry",	T_STR,		NULL, "Font foundry name" },
+/*	{ XFT_FTFACE,		"ftface",	T_FT_FACE,	NULL, "FreeType face" }, */
+/*	{ XFT_GLOBAL_ADVANCE,	"globaladv",	T_BOOL,		NULL, "Global advance data" }, */
+/*	{ XFT_HINTING,		"hint",		T_BOOL,		NULL, "Hinting" }, */
+	{ XFT_INDEX,		"index",	T_INT,		NULL, "Index of font from resource" },
+/*	{ XFT_LANG,		"lang",		T_STR,		NULL, "Languages" }, */
+	{ XFT_MATRIX,		"matrix",	T_BOOL,		NULL, "Modification matrix" },
+	{ XFT_MINSPACE,		"minspace",	T_BOOL,		NULL, "Line space leading" },
+	{ XFT_OUTLINE,		"outline",	T_BOOL,		NULL, "Outlines" },
+	{ XFT_PIXEL_SIZE,	"pixelsize",	T_DBL,		NULL, "Pixel size" },
+	{ XFT_RASTERIZER,	"rasterizer",	T_STR,		NULL, "Rasterizer" },
+	{ XFT_RGBA,		"rgba",		T_INT,		NULL, "Subpixel geometry" },
+	{ XFT_SCALABLE,		"scalable",	T_BOOL,		NULL, "Scalibility" },
+	{ XFT_SCALE,		"scale",	T_DBL,		NULL, "Point to pixel scale conversion" },
+	{ XFT_SIZE,		"size",		T_DBL,		NULL, "Point size" },
+	{ XFT_SLANT,		"slant",	T_INT,		NULL, "Italics/roman/oblique" },
+	{ XFT_SPACING,		"spacing",	T_INT,		NULL, "Proprotional/monospace/charcell" },
+	{ XFT_STYLE,		"style",	T_STR,		NULL, "Font style (overrides weight/slant)" },
+/*	{ XFT_VERTICAL_LAYOUT,	"vertlayout",	T_BOOL,		NULL, "Vertical layout" }, */
+	{ XFT_WEIGHT,		"weight",	T_INT,		NULL, "Light/medium/bold" },
 };
+#define NUM_PROPS (sizeof(props) / sizeof(props[0]))
 
 XtActionsRec actions[] = {
 	{ "Quit", (void *)exit }
@@ -66,7 +73,6 @@ struct {
 	Cursor cursor;
 } appres;
 
-#define NUM_PROPS (sizeof(props) / sizeof(props[0]))
 
 void requery(Widget, XtPointer, XtPointer);
 void cb_quit(Widget, XtPointer, XtPointer);
@@ -76,22 +82,25 @@ int
 main(int argc, char *argv[])
 {
 	int i;
+	XftDraw *xd;
 
 	XtSetLanguageProc(NULL, (XtLanguageProc) NULL, NULL);
 	top = XtAppInitialize(&appctx, "XftFontSel", NULL, 0, &argc, argv, NULL, NULL, ZERO);
 	XtAppAddActions(appctx, actions, XtNumber(actions));
-    
+
+//	xd = XftDrawCreate();
+
 	pane 		= XtCreateManagedWidget("pane", panedWidgetClass, top, NULL, 0);
 	command_box 	= XtCreateManagedWidget("command_box", formWidgetClass, pane, NULL, 0);
 	field_box 	= XtCreateManagedWidget("field_box", boxWidgetClass, pane, NULL, 0);
-	
+
 	for (i = 0; i < NUM_PROPS; i++) {
-		props[i].prop_widget = XtCreateManagedWidget(props[i].prop_name, labelWidgetClass, field_box, NULL, ZERO);
-		XtAddCallback(props[i].prop_widget, XtNcallback, requery, NULL);
+		props[i].prop_widget = XtCreateManagedWidget(props[i].prop_name, menuButtonWidgetClass, field_box, NULL, ZERO);
+		XtAddCallback(props[i].prop_widget, XtNcallback, requery, &props[i]);
 	}
 
-	quit_button	= XtCreateManagedWidget("quit_button", commandWidgetClass, command_box, NULL, 0);
-	copy_button	= XtCreateManagedWidget("copy_button", commandWidgetClass, command_box, NULL, 0);
+	quit_button	= XtCreateManagedWidget("Quit", commandWidgetClass, command_box, NULL, 0);
+	copy_button	= XtCreateManagedWidget("Copy To Clipboard", commandWidgetClass, command_box, NULL, 0);
 	count_label	= XtCreateManagedWidget("count_label", labelWidgetClass, command_box, NULL, 0);
 
 	XtAddCallback(quit_button, XtNcallback, cb_quit, NULL);
@@ -124,4 +133,5 @@ cb_copy(Widget w, XtPointer closure, XtPointer callData)
 void
 requery(Widget w, XtPointer closure, XtPointer callData)
 {
+	printf("requiried\n");
 }
